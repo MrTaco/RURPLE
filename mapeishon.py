5ad3aeb3b55c2471c30df8fadc1b29e797cb407f
Erick Castillo <castilloherrera@yahoo.com>
2016-08-22 08:33:28 -0600

    mapa e instrucciones

---
+++ mapeishon.py
@@ -0,0 +1,6 @@
+from rurple import cargar_mapa, cargar_instrucciones
+
+
+nombre_mapa = input("Ingrese el nombre del mapa: ")
+print(cargar_mapa(nombre_mapa))
+print(cargar_instrucciones())
--- rurple.py
+++ rurple.py
@@ -1,5 +1,15 @@
-def carga_mapa(mapeishon):
-	f=open(Mapas/mapa1.txt, "r")
-	mapa=list(f)
-	return mapa
-print(carga_mapa)
+def cargar_mapa(nombre_mapa):
+	mapa = open(nombre_mapa, "r")
+	resultado = []
+	for i in mapa:
+		resultado.append(list(i.strip()))
+	mapa.close()
+	return resultado
+
+def cargar_instrucciones():
+	inst = open(programa1.txt, "r")
+	instrucciones = []
+	for i in inst:
+		instrucciones.append(list(i))
+	programa1.close()
+	return instrucciones
