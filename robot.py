fffd720fec36425097c6191f98ca6a343ccfaaf6
Erick Castillo <castilloherrera@yahoo.com>
2016-09-06 08:22:52 -0600

    avance

--- robot.py
+++ robot.py
@@ -7,14 +7,25 @@ class Robot():
 		self.mapa = None
 
 	def Move(self):
-		if self.direccion=="^":
+		if self.direccion=="UP":
 			self.y-=1
-		elif self.direccion==">":
+		elif self.direccion=="RIGHT":
 			self.x+=1
-		elif self.direccion=="v":
+		elif self.direccion=="DOWN":
 			self.y+=1
 		else:
 			self.x-=1
+        if self.x < 0:
+            self.x = 0
+
+        if self.x >= self.mapa.ancho:
+            self.x = self.mapa.ancho - 1
+
+        if self.y < 0:
+            self.y = 0
+
+        if self.y >= self.mapa.alto:
+            self.y = self.mapa.alto - 1
 
 	def dibujar(self):
 		if self.direccion=="UP":
@@ -24,8 +35,10 @@ class Robot():
 		elif self.direccion=="DOWN":
 			return "v"
 		else:
-			self.direccion=="<"
+			return "<"
 
+	def asignar_mapa(self):
+		self.mapa=mapa
 	
 
 
