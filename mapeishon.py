85618ab076c7460f06708058b708e00a6b2c65f5
Erick Castillo <castilloherrera@yahoo.com>
2016-09-06 08:11:06 -0600

    avance

--- mapeishon.py
+++ mapeishon.py
@@ -3,23 +3,37 @@ class Mapa(object):
 		self.ancho=ancho
 		self.alto=alto
 		self.monedas=[]
-	
+		self.robot= None
 	def asignar_robot(self, robot):
 		self.robot = robot
 
 	def agregar_moneda(self, moneda):
 		self.monedas.append(moneda)
-lista_mapa=[....]
-mapa=Mapa(len(lista_mapa[0],lista_mapa))
+
+	def contar(self, x, y):
+    contador = 0
+    for moneda in self.monedas:
+        if moneda.x == x and moneda.y == y:
+           contador += 1
+
+
+        return contador
+
 	def dibujar_mapa(self):
-		for y in range(alto):
-			for x in range(ancho):
-				if lista_mapa[y][x]=="*":
-					robot=Robot[x][y]
-				elif lista_mapa[y][x] == "0":
-					pass
-				else:
-					cantidad=int(lista_mapa[y][x])
-					for i in range(cantidad):
-						moneda=Moneda(x,y)
-						mapa_agregar=moneda(moneda)
+        resultado = ""
+
+        for y in range(self.altura):
+            for x in range(self.ancho):
+                if x == self.robot.x and y == self.robot.y:
+                    resultado += self.robot.dibujar()
+
+                elif self.contar_monedas_en(x, y) > 0:
+                    resultado += self.contar_monedas_en(x, y)
+                else:
+                    resultado += " "
+
+            resultado += "\n"
+
+        return resultado
+
+        
